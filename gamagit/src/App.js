4bdddbda2489b5537d45697bcfac853a03b143e5
Edu Reis <edurei75@gmail.com>
2021-07-21 22:13:48 -0300

    Teste de React
    Conceitos práticos de React

---
+++ gamagit/src/App.js
@@ -0,0 +1,14 @@
+import React,{useState} from 'react';
+
+
+function App(props) {
+
+  return (
+    <>
+    <input name ="usuario" placeholder="usuario" className="usuario" id="usuario"/>
+    <button type="button">Pesquisa</button>
+  </>
+  );
+}
+
+export default App;
